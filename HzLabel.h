95b23ce324e98455a38833125080e6aaa2f578ee
kevinva <kevinva@sina.com>
2013-09-29 23:56:21 +0800

    first commit

---
+++ HzLabel.h
@@ -0,0 +1,20 @@
+//
+//  HzLabel.h
+//  HzTest2
+//
+//  Created by 何 峙 on 13-9-29.
+//  Copyright (c) 2013年 何 峙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HzLabel : UIView
+
+@property (nonatomic, assign) CGFloat lineSpace;
+@property (nonatomic, retain) UIFont *font;
+@property (nonatomic, retain) NSString *text;
+
+
++ (CGFloat)boundingHeightForWidth:(CGFloat)w string:(NSString *)text font:(UIFont *)font lineSpace:(CGFloat)space;
+
+@end
