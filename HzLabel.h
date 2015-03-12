43aa00daa99f29a1410d279501b007a583d32474
kevin <kevinva@sina.com>
2015-03-12 14:46:07 +0800

    Add IBDesignalbe and IBInspectable feature for HzLabel

--- HzLabel.h
+++ HzLabel.h
@@ -10,12 +10,13 @@
 
 @class HzLabelBuilder;
 
+IB_DESIGNABLE
 @interface HzLabel : UIView
 
-@property (nonatomic, assign) CGFloat lineSpace;
+@property (nonatomic, assign) IBInspectable CGFloat lineSpace;
+@property (nonatomic, retain) IBInspectable NSString *text;
+@property (nonatomic, retain) IBInspectable UIColor *textColor;
 @property (nonatomic, retain) UIFont *font;
-@property (nonatomic, retain) NSString *text;
-@property (nonatomic, retain) UIColor *textColor;
 
 + (instancetype)labelWithBuilder:(void (^)(HzLabelBuilder *builder))block;
 + (CGFloat)boundingHeightForWidth:(CGFloat)w string:(NSString *)text font:(UIFont *)font lineSpace:(CGFloat)space;
